1bb2d37f984b21c9dd536f05d23ea31a808bac64
shahbaz <quazi.shahbaz95@gmail.com>
2021-12-10 13:46:14 +0530

    Add a active class to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export class Navbar extends Component {
     render() {
@@ -7,36 +7,36 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-warning">
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/general">RealNews</Link>
+                        <NavLink className="navbar-brand" to="/general">RealNews</NavLink>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link" aria-current="page" to="/general">Home</Link>
+                                    <NavLink className="nav-link" aria-current="page" to="/general" activeClassName="active">Home</NavLink>
                                 </li>
 
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/business">Business</Link>
+                                    <NavLink className="nav-link"  to="/business" activeClassName="active">Business</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
+                                    <NavLink className="nav-link" to="/entertainment" activeClassName="active">Entertainment</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/general">General</Link>
+                                    <NavLink className="nav-link" to="/general" activeClassName="active">General</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/health">Health</Link>
+                                    <NavLink className="nav-link" to="/health" activeClassName="active">Health</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/science">Science</Link>
+                                    <NavLink className="nav-link" to="/science" activeClassName="active">Science</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/sports">Sports</Link>
+                                    <NavLink className="nav-link" to="/sports" activeClassName="active">Sports</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/technology">Technology</Link>
+                                    <NavLink className="nav-link"  to="/technology" activeClassName="active">Technology</NavLink>
                                 </li>
                             </ul>
 
