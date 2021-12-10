3876c20741ef6ce06223f885d9d88e9ea44e1cea
shahbaz <quazi.shahbaz95@gmail.com>
2021-12-11 02:45:34 +0530

    changed the font-size in the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,12 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-warning">
                     <div className="container-fluid">
-                        <NavLink className="navbar-brand" to="/general"><img src="/logo.png" alt="RealNews" /></NavLink>
+                        <NavLink className="navbar-brand mx-3" style={{fontSize:"25px"}} to="/general"><img src="/logo.png" alt="RealNews" />RealNews</NavLink>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <div className="collapse navbar-collapse mx-5" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-1" style={{fontSize:"20px"}}>
                                 <li className="nav-item">
                                     <NavLink className="nav-link" aria-current="page" to="/general" activeclassname="active">Home</NavLink>
                                 </li>
